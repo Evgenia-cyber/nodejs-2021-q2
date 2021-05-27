1557cfded23a2f81c90a537c98cfe21ac14ac541
Evgenia Kurineva <kurineva85@mail.ru>
2021-05-27 11:54:17 +0300

    docs: add description for board.service and board.memory.repository using JSDoc

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -1,9 +1,29 @@
+/**
+ * @module boardRepository
+ */
+
+/**
+ * A board's data from request body
+ * @typedef {Object} BoardDataFromRequestBody
+ * @property {string} title - The board's title.
+ * @property {Array<Column>} columns - The board's columns.
+ */
+
 const { BOARDS } = require('../data/data.js');
 const Board = require('./board.model');
 const Column = require('./column.model');
 
+/**
+ * Get all boards
+ * @returns {Promise<Board[]>} Promise object represents an array of all boards or an empty array
+ */
 const getAll = async () => BOARDS;
 
+/**
+ * Create new board
+ * @param {BoardDataFromRequestBody} body - Information about the board
+ * @returns {Promise<Board|null>} Promise object represents new board or null
+ */
 const create = async (body) => {
   const { title, columns } = body;
   if (!title || !columns) {
@@ -17,8 +37,19 @@ const create = async (body) => {
   return newBoard;
 };
 
+/**
+ * Get board by board's id
+ * @param {string} id - The board's id.
+ * @returns {Promise<Board|undefined>} Promise object represents board or undefined
+ */
 const getById = async (id) => BOARDS.find((board) => board.id === id);
 
+/**
+ * Update board by board's id
+ * @param {string} id - The board's id.
+ * @param {BoardDataFromRequestBody} body - New information about the board
+ * @returns {Promise<Board|null>} Promise object represents updated board or null
+ */
 const update = async (id, body) => {
   const { title, columns } = body;
   if (!title || !columns) {
@@ -32,6 +63,11 @@ const update = async (id, body) => {
   return BOARDS[index];
 };
 
+/**
+ * Delete board by board's id
+ * @param {string} id - The board's id.
+ * @returns {Promise<null|true>} Promise object represents null or true
+ */
 const del = async (id) => {
   const index = BOARDS.findIndex((board) => board.id === id);
   if (index < 0) {
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,9 +1,49 @@
+/**
+ * @module boardService
+ */
+
+/**
+ * A board's data from request body
+ * @typedef {Object} BoardDataFromRequestBody
+ * @property {string} title - The board's title.
+ * @property {Array<Column>} columns - The board's columns.
+ */
+
 const boardsRepo = require('./board.memory.repository');
 
+/**
+ * Get all boards
+ * @returns {function(): Promise<Board[]>} - Returns a function that returns a Promise object that is an array of all boards or an empty array
+ */
 const getAll = () => boardsRepo.getAll();
+
+/**
+ * Create new board
+ * @param {BoardDataFromRequestBody} body - A board's data from request body
+ * @returns {function(): Promise<Board|null>} - Returns a function that returns a Promise object that is a new board or null
+ */
 const create = (body) => boardsRepo.create(body);
+
+/**
+ * Get board by board's id
+ * @param {string} id - The board's id
+ * @returns {function(): Promise<Board|undefined>} - Returns a function that returns a Promise object that is a board or undefined
+ */
 const getById = (id) => boardsRepo.getById(id);
+
+/**
+ * Update board by board's id
+ * @param {string} id - The board's id.
+ * @param {BoardDataFromRequestBody} body - New board's data from request body
+ * @returns {function(): Promise<Board|null>} - Returns a function that returns a Promise object that is a updated board or null
+ */
 const update = (id, body) => boardsRepo.update(id, body);
+
+/**
+ * Delete board by board's id
+ * @param {string} id - The board's id.
+ * @returns {function(): Promise<null|true>} Returns a function that returns a Promise object that is null or true
+ */
 const del = (id) => boardsRepo.del(id);
 
 module.exports = { getAll, getById, create, update, del };
