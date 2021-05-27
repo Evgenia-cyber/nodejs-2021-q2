6a517fc341c21239af73e0b5d4632c11d1ab8a0c
Evgenia Kurineva <kurineva85@mail.ru>
2021-05-27 13:34:39 +0300

    docs: add task.memory.repository and task.service description using JSDoc

--- src/resources/tasks/task.memory.repository.js
+++ src/resources/tasks/task.memory.repository.js
@@ -1,8 +1,31 @@
+/**
+ * @module taskRepository
+ */
+
+/**
+ * A task's data from request body
+ * @typedef {Object} TaskDataFromRequestBody
+ * @param {string} task.title - The task's title.
+ * @param {number} task.order - The task's order.
+ * @param {string} task.description - The task's description.
+ * @param {string|null} task.userId - The task's user id.
+ */
+
 let { TASKS: tasks } = require('../data/data.js');
 const Task = require('./task.model.js');
 
+/**
+ * Get all tasks
+ * @returns {Promise<Task[]>} Promise object represents an array of all tasks or an empty array
+ */
 const getAll = async () => tasks;
 
+/**
+ * Create new task
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {TaskDataFromRequestBody} body - Information about the task
+ * @returns {Promise<Task|null>} Promise object represents new task or null
+ */
 const create = async (boardId, body) => {
   const { title, order, description, userId, columnId } = body;
   if (!title || !description) {
@@ -20,9 +43,22 @@ const create = async (boardId, body) => {
   return newTask;
 };
 
+/**
+ * Get task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @returns {Promise<Task|undefined>} Promise object represents task or undefined
+ */
 const getById = async (boardId, taskId) =>
   tasks.find((task) => task.boardId === boardId && task.id === taskId);
 
+/**
+ * Update task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @param {TaskDataFromRequestBody} body - New information about the task
+ * @returns {Promise<Task|null>} Promise object represents updated task or null
+ */
 const update = async (boardId, taskId, body) => {
   const { title, order, description, userId, columnId } = body;
   if (!title || !description) {
@@ -46,6 +82,12 @@ const update = async (boardId, taskId, body) => {
   return tasks[index];
 };
 
+/**
+ * Delete task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @returns {Promise<null|true>} Promise object represents null or true
+ */
 const del = async (boardId, taskId) => {
   const index = tasks.findIndex(
     (task) => task.boardId === boardId && task.id === taskId
@@ -57,20 +99,23 @@ const del = async (boardId, taskId) => {
   return true;
 };
 
+/**
+ * Delete tasks by board's id, when this board deleted
+ * @param {string} boardId - The board's id - ID of the board this tasks belongs to
+ * @returns {Promise<true>} Promise object represents true
+ */
 const deleteTasksWhenBoardDeleted = async (boardId) => {
   tasks = tasks.filter((task) => task.boardId !== boardId);
   return true;
 };
 
+/**
+ * Update tasks by user's id - when this user deleted, all his tasks would be updated to put userId = null
+ * @param {string} userId - The user's id - ID of the user this tasks belongs to
+ * @returns {Promise<true>} Promise object represents true
+ */
 const updateTasksWhenUserDeleted = async (userId) => {
-  // tasks.forEach((task) => {
-  //   if (task.userId === userId) {
-  //     // eslint-disable-next-line no-param-reassign
-  //     task.userId = null;
-  //   }
-  // });
   tasks = tasks.map((task) => {
-    // const copyTask = { ...task };
     const copyTask = task;
     if (copyTask.userId === userId) {
       copyTask.userId = null;
--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -1,13 +1,70 @@
+/**
+ * @module taskService
+ */
+
+/**
+ * A task's data from request body
+ * @typedef {Object} TaskDataFromRequestBody
+ * @param {string} task.title - The task's title.
+ * @param {number} task.order - The task's order.
+ * @param {string} task.description - The task's description.
+ * @param {string|null} task.userId - The task's user id.
+ */
+
 const tasksRepo = require('./task.memory.repository');
 
+/**
+ * Get all tasks
+ * @returns {function(): Promise<Task[]>} - Returns a function that returns a Promise object that is an array of all tasks or an empty array
+ */
 const getAll = () => tasksRepo.getAll();
+
+/**
+ * Create new task
+ * @param {TaskDataFromRequestBody} body - A task's data from request body
+ * @returns {function(): Promise<Taks|null>} - Returns a function that returns a Promise object that is a new task or null
+ */
 const create = (boardId, body) => tasksRepo.create(boardId, body);
+
+/**
+ *  Get task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @returns {function(): Promise<Task|undefined>} - Returns a function that returns a Promise object that is a task or undefined
+ */
 const getById = (boardId, taskId) => tasksRepo.getById(boardId, taskId);
+
+/**
+ * Update task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @param {TaskDataFromRequestBody} body - New task's data from request body
+ * @returns {function(): Promise<Task|null>} - Returns a function that returns a Promise object that is a updated task or null
+ */
 const update = (boardId, taskId, body) =>
   tasksRepo.update(boardId, taskId, body);
+
+/**
+ * Delete task by board's and task's ids
+ * @param {string} boardId - The board's id - ID of the board this task belongs to
+ * @param {string} taskId - The task's id
+ * @returns {function(): Promise<null|true>} Returns a function that returns a Promise object that is null or true
+ */
 const del = (boardId, taskId) => tasksRepo.del(boardId, taskId);
+
+/**
+ * Delete tasks by board's id, when this board deleted
+ * @param {string} boardId - The board's id - ID of the board this tasks belongs to
+ * @returns {function():Promise<true>} Returns a function that returns a Promise object that is true
+ */
 const deleteTasksWhenBoardDeleted = (boardId) =>
   tasksRepo.deleteTasksWhenBoardDeleted(boardId);
+
+/**
+ * Update tasks by user's id - when this user deleted, all his tasks would be updated to put userId = null
+ * @param {string} userId - The user's id - ID of the user this tasks belongs to
+ * @returns {function():Promise<true>} Returns a function that returns a Promise object that is true
+ */
 const updateTasksWhenUserDeleted = (taskId) =>
   tasksRepo.updateTasksWhenUserDeleted(taskId);
 
