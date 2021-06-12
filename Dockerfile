3ffb2dc73b16fdb30445c19733072264cf760cac
Evgenia Kurineva <kurineva85@mail.ru>
2021-06-12 21:27:14 +0300

    feat: add docker-composer.yml

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /user/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 8080
+EXPOSE ${PORT}
 CMD ["npm","run","dev"]
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import app from './app';
 import { PORT } from './common/config';
 
+console.log(12356);
+console.log(PORT);
 app.listen(PORT, () =>
   // eslint-disable-next-line no-console
   console.log(`App is running on http://localhost:${PORT}`)
